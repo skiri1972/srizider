8da631d96e5403225ce4bafd3ca5182562d9127a
skiri1972 <skiri1972@gmail.com>
2026-06-22 21:56:44 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/* ============================================================
-   KONFIGURACIJA - API TOKEN
-   ============================================================
-   OVAJ FAJL JE U .gitignore - NEĆE BITI PUSH-OVAN NA GITHUB!
-   ============================================================ */
-
-// Tvoj Hugging Face API token
-const HF_API_TOKEN = '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';
